cdf80828a2010256589cdeef955905c75a432f0e
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2023-07-29 13:10:36 +0200

    Added Sentry monitoring and tracing

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "http://31b22eb71c4d4b25a48ce7db15be4b0e@192.168.1.102:9000/2",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
