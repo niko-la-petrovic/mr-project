696ce1e4240f7cdcddc34a31788567fc008ac2c9
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2023-07-30 21:53:55 +0200

    Refactor

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -18,7 +18,7 @@ export function Button({ children, tooltip, ...props }: ButtonProps) {
   return (
     <>
       <button
-        className="px-4 py-2 rounded-md hover:bg-primary-200 active:bg-primary-400 dark:hover:bg-primary-700 dark:active:bg-primary-900 transition-colors duration-200 ease-in-out"
+        className="px-4 py-2 rounded-md bg-white dark:bg-slate-800 hover:bg-primary-200 active:bg-primary-400 dark:hover:bg-primary-700 dark:active:bg-primary-900 transition-colors duration-200 ease-in-out"
         {...props}
         {...(tooltipProvided
           ? {
--- src/components/graph/customImageFlowNode.tsx
+++ src/components/graph/customImageFlowNode.tsx
@@ -1,43 +1,55 @@
 import { Handle, Position, useReactFlow } from "reactflow";
 import {
+  ImageFlowData,
   ImageFlowEdge,
+  ImageFlowEdgeData,
   ImageFlowNode,
   ImageFlowNodeProps,
 } from "@/types/domain";
+import _, { curry } from "lodash";
 
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { useCallback } from "react";
 
+const nodeTransform = (
+  nodes: ImageFlowNode[],
+  id: string,
+  transformation: (node: ImageFlowNode) => ImageFlowNode
+): ImageFlowNode[] => {
+  return nodes.map((node) => {
+    if (node.id === id) {
+      return transformation(node);
+    }
+    return node;
+  });
+};
+
 export function CustomImageFlowNode({ id, data }: ImageFlowNodeProps) {
-  const { setNodes } = useReactFlow<ImageFlowNode, ImageFlowEdge>();
+  const { setNodes } = useReactFlow<ImageFlowData, ImageFlowEdgeData>();
   const updateNodeLabel = useCallback(
-    (id: string, label: string) => {
-      setNodes((prevNodes) =>
-        prevNodes.map((node) => {
-          if (node.id === id) {
-            return {
-              ...node,
-              data: {
-                ...node.data,
-                label,
-              },
-            };
-          }
-          return node;
-        })
-      );
-    },
-    [setNodes]
+    (label: string) =>
+      setNodes((nodes) =>
+        nodeTransform(nodes, id, (node) => ({
+          ...node,
+          data: { ...node.data, label },
+        }))
+      ),
+    [id, setNodes]
   );
 
   return (
     <>
       <Handle type="target" position={Position.Top} />
       <div className="flex flex-col justify-center items-center p-4  bg-white relative rounded gap-2 border-2 border-gray-400 hover:border-primary-600 transition-all duration-300 ease-in-out">
-        <span className="absolute top-0 left-0 text-xs font-semibold">
+        <span className="absolute top-0 left-0 text-xs font-semibold pl-2 pt-2">
           {id}
         </span>
-        <span className="text-xs font-semibold">{data.label}</span>
+        <input
+          className="w-full text-center text-xs font-semibold"
+          type="text"
+          value={data.label}
+          onChange={(e) => updateNodeLabel(e.target.value)}
+        />
         {data.content?.memo ? (
           <div className="s-64 relative">
             <Image
